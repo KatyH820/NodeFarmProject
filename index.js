1bc689ae26b3749aee570c00e78d33d70c188093
Katy Huang <Katyh1@uci.edu>
2022-12-13 21:46:49 -0800

    save the replaceTemplate function to a module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http=require('http');
 const url= require('url');
-
+const replaceTemplate = require('./modules/replaceTemplate');
 ////////////////////////////
 ///Server
 
@@ -12,21 +12,6 @@ const overview = fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8')
 const product = fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8');
 const card = fs.readFileSync(`${__dirname}/templates/card.html`,'utf-8');
 
-const replaceTemplate = (temp,product)=>{
-    // /{}/g wrap the thing in a regular expression and use the g flag on it, which mean
-    //global and so this will make it so that all of these placeholders will get replaced,
-    //and not just the first one that occurs
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-    output = output.replace(/{%IMAGE%}/g,product.image);
-    output = output.replace(/{%PRICE%}/g,product.price);
-    output = output.replace(/{%FROM%}/g,product.from);
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g,product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g,product.description);
-    output = output.replace(/{%ID%}/g,product.id);
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
 //createServer() will accept a callback function, which will be fired off each time 
 //a new request hits our server. The callback function get access to request&response variable
 const server = http.createServer((req,res)=>{
